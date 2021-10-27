fd08f6022e3588cf647e3ca6a0c5d4884b81b946
Samir <samir.beall@gmail.com>
2021-10-27 16:21:18 -0700

    e

--- tuning.py
+++ tuning.py
@@ -16,52 +16,88 @@ axis = odrv1.axis1
 
 def move(dist = 5):
     axis.controller.input_pos = 0
-    time.sleep(5)
+    time.sleep(2)
 
-    axis.controller.input_po = dist
-    time.sleep(5)
+    axis.controller.input_pos = dist
 
-start_liveplotter(lambda:[axis.encoder.pos_estimate, axis.controller.pos_setpoint])
+def move2(t = 1):
+    startpos = axis.encoder.pos_estimate
+
+    axis.controller.input_vel = 3
+    time.sleep(t)
+    axis.controller.input_vel = -3
+    time.sleep(t)
+    axis.controller.input_pos = startpos
+
+
+start_liveplotter(lambda:[axis.encoder.pos_estimate, axis.controller.input_pos])
 
 
 axis.requested_state =  AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 
 while axis.current_state != AXIS_STATE_IDLE:
     pass
+time.sleep(1)
 
 axis.controller.config.vel_limit = 20
 axis.controller.config.enable_overspeed_error = False
 
 axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-axis.controller.control_mode = CONTROL_MODE_POSITION_CONTROL
+axis.controller.config.control_mode = CONTROL_MODE_POSITION_CONTROL
+axis.controller.config.input_mode = INPUT_MODE_PASSTHROUGH
+
+axis.controller.input_pos = 1
+
+time.sleep(1)
+
+
+print(axis.controller.input_pos)
+print(axis.controller.pos_setpoint)
+print(axis.encoder.pos_estimate)
+
+
+
+
 
 while True:
     print(f"""
-    vel_gain (vg) = {axis.controller.config.vel_gain}\n
-    pos_gain (pg) = {axis.controller.config.pos_gain}\n
-    vel_integrator_gain (vig) = {axis.controller.config.vel_integrator_gain}\n""")
+    vel_gain (v) = {axis.controller.config.vel_gain}
+    pos_gain (p) = {axis.controller.config.pos_gain}
+    vel_integrator_gain (i) = {axis.controller.config.vel_integrator_gain}""")
 
     text = input()
 
     try:
 
-        if text == "default":
-            axis.controller.config.vel_gain = 20
-            axis.controller.config.pos_gain = .16
+        if text == "d":
+            axis.controller.config.vel_gain = .16
+            axis.controller.config.pos_gain = 20
             axis.controller.config.vel_integrator_gain = .32
 
-        elif text == "move":
-            move()
+
+        elif text[0:2] == 'm2':
+
+            val = float(text.split(" ")[1])
+            print(f"moving vel {val}")
+            move2(float(val))
+
+        elif text[0] == 'm':
+
+            val = float(text.split(" ")[1])
+            print(f"moving {val}")
+            move(float(val))
+
+
             
         else:
-            target, val = text.split("=")
-            val = int(val)
+            target, val = text.split(" ")
+            val = float(val)
 
-            if target == "vg":
+            if target == "v":
                 axis.controller.config.vel_gain = val
-            elif target == "pg":
+            elif target == "p":
                 axis.controller.config.pos_gain = val
-            elif target == "vig":
+            elif target == "i":
                 axis.controller.config.vel_integrator_gain = val
             
 
