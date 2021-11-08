5e86c70f32026c857f93cb0b446c7b5c84e692ef
Samir <Samir.beall@gmail.com>
2021-11-08 13:32:43 -0800

    e

--- stupidcli.py
+++ stupidcli.py
@@ -1,9 +1,16 @@
 import controller
 
-controller = controller.Controller()
 
-while True:
-    x = int(input("x plz "))
-    y = int(input("y plz "))
-    controller.trap_move(x, y)
-    
+def main():
+    controller = controller.Controller()
+    while True:
+        x = int(input("x plz "))
+        y = int(input("y plz "))
+        controller.trap_move(x, y)
+
+if __name__ == "__main__":
+    try: 
+        main()
+    except Exception:
+        controller.trap_move(3,3)
+        import guess_what_this_does
