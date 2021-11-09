40e1d3faeb427d6b65a0efc4db8353e20b8ded7a
LeadFreeCandy <39067807+LeadFreeCandy@users.noreply.github.com>
2021-11-08 19:35:59 -0800

    added shit

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 def main():
     import gantry
+    import pickle
+
+
+    def pen_up():
+        pass
+    def pen_down():
+        pass
+    
+    segments = None
+
+    with open("path.pickle", "rb") as file:
+        segments = pickle.load(file)
+        print(segments)
+
+
+
     gantry = gantry.Gantry()
     gantry.startup()
 
-
     
+    for seg in segments:
+        for point in seg:
+            gantry.trap_move(*point)
+
+
     
 
 if __name__ == "__main__":
