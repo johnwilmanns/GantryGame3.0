aa241d4e76bcc236f5e8becbab113c29d6aa9cc2
Samir <samir.beall@gmail.com>
2021-12-14 10:32:14 -0800

    init commit

--- main.py
+++ main.py
@@ -102,7 +102,6 @@ def main():
     # with open("path.pickle", "rb") as file:
     #     segments = pickle.load(file)
     #     # print(segments)
-
     segments, freq = process_face("small_obama.jpg", blur_radius=11, lower_thresh=10,
                                   upper_thresh=50, segmentSplitDistance=15, areaCut=3,
                                   minNumPixels=15, max_accel=20, max_lr=1, turn_vel_multiplier=1, freq=120,
