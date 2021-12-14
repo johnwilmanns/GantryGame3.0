ffb314459849dd9e2c0dfa1e925ceb853573b891
Samir <samir.beall@gmail.com>
2021-12-14 10:38:38 -0800

    commit andpush again because samir screwed up git

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def main():
     #     # print(segments)
     segments, freq = process_face("small_obama.jpg", blur_radius=11, lower_thresh=10,
                                   upper_thresh=50, segmentSplitDistance=15, areaCut=3,
-                                  minNumPixels=15, max_accel=20, max_lr=1, turn_vel_multiplier=1, freq=120,
+                                  minNumPixels=15, max_accel=10, max_lr=.01, turn_vel_multiplier=1, freq=60,
                                   plot_steps=False)
 
 
