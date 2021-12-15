eaaf194d94db0cf8fc7723e8060ad5dbc22ffc16
johnwilmanns <johnwilmanns@gmail.com>
2021-12-15 13:17:57 -0800

    houghlines kinda works, rewriting it tho...

--- utilities.py
+++ utilities.py
@@ -8,7 +8,7 @@ def get_blank_image(rows,cols,color = False):
     if color:
         return np.zeros((rows, cols, 3), np.uint8)
     else:
-        y = np.zeros((rows, cols, 1), np.uint8)
+        return np.zeros((rows, cols, 1), np.uint8)
 
 def copy_blank(image):
     im = image.copy()
