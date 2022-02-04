18b2b3186fdbe8ae9ef75a936eebfdd4226625b8
johnwilmanns <johnwilmanns@gmail.com>
2022-02-04 13:03:05 -0800

    added testing shit

--- preview.py
+++ preview.py
@@ -1,7 +1,7 @@
 from full_face_processing import process_face, plot_path_full
 
 
-segments, freq = process_face("C:/Users/Samir/OneDrive/Documents/Drawing Bot/GantryGame3.0/GantryGame3.0/small_obama.jpg", blur_radius = 11, lower_thresh = 10,
+segments, freq = process_face("small_obama.jpg", blur_radius = 11, lower_thresh = 10,
         upper_thresh = 50, segmentSplitDistance=15, areaCut = 3,
         minNumPixels = 15, max_accel = 5, max_lr = 1, turn_vel_multiplier = 1, freq = 60, plot_steps = False)
 
