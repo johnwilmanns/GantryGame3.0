510ca07ad7f86de8748405a883e83c02d59c32d3
johnwilmanns <johnwilmanns@gmail.com>
2022-04-25 13:25:02 -0700

    wrote thing.py

--- thing.py
+++ thing.py
@@ -1,10 +1,48 @@
-
-#find high frequencies in image
 import cv2
-import numpy as np
-import math
-import os
-import sys
-import argparse
+import time
 
-#find high frequencies in image
+cam = cv2.VideoCapture(0, cv2.CAP_V4L)
+cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+
+FULL_RES = True
+
+if FULL_RES:
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+# assert width == 1920 and height == 1080, "the res is wrong"
+
+
+print(width, height)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    t0 = time.time()
+
+    ret, frame = cam.read()
+    # if not ret:
+    #     print("failed to grab frame")
+    #     break
+    # cv2.imshow("test", frame)
+
+    # k = cv2.waitKey(1)
+    # if k%256 == 27:
+    #     # ESC pressed
+    #     print("Escape hit, closing...")
+    #     break
+    # elif k%256 == 32:
+    #     # SPACE pressed
+    #     img_name = "opencv_frame_{}.png".format(img_counter)
+    #     cv2.imwrite(img_name, frame)
+    #     print("{} written!".format(img_name))
+    #     img_counter += 1
+    print(f"recording at {1 / (time.time() - t0)}hz")
+cam.release()
+
+cv2.destroyAllWindows()
