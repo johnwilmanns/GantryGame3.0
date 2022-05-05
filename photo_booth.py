ea18046ef9fab8fa5c48c5950ae85dbdb5a27aa4
johnwilmanns <johnwilmanns@gmail.com>
2022-05-05 10:52:42 -0700

    has multiple things and gets edges

--- photo_booth.py
+++ photo_booth.py
@@ -4,16 +4,36 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 import time
 
+
+import cv2
+import image_processing
+
+global edges_image
+
+# may allah forgive me for how I am dealing with the images
+
 class MainWindow(Screen):
     def capture(self):
+        global edges_image
         '''
         Function to capture the images and give them the names
         according to their captured time and date.
         '''
         camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
+        camera.export_to_png("image.png")
+
         print("Captured")
+
+        filename = "image.png"
+        frame = cv2.imread(filename)
+
+        segments = image_processing.process_combo_raw(frame)
+
+        #     segments = trajectory_planning.calc_path(segments, 5, .01, 1, 120)
+        edges_image = image_processing.plot_segments(segments)
+
+        cv2.imwrite("edges_image.jpg", edges_image)
+
 
 
 
