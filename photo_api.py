53b05c653960d7a987a095978336039aacc130c6
Samir <Samir.beall@gmail.com>
2022-05-05 17:24:29 -0700

    e

--- photo_api.py
+++ photo_api.py
@@ -42,6 +42,7 @@ SCOPES = 'https://www.googleapis.com/auth/photoslibrary.readonly'
 
 store = file.Storage('token-for-google.json')
 creds = store.get()
+# creds = None
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
@@ -53,27 +54,44 @@ google_photos = build('photoslibrary', 'v1', http=creds.authorize(Http()), stati
 # for item in items:
 #         print(u'{0} ({1})'.format(item['title'].encode('utf8'), item['id']))
 
-images = {}
-
-
-
-counter = 0 
+# images = {}
 nextpagetoken = 'Dummy'
-while nextpagetoken != '':
-    nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
-    # results = google_photos.mediaItems().list(query={"pageSize": 100, "pageToken": nextpagetoken}).execute()
-    results = google_photos.mediaItems().list(pageSize=100).execute()
-    # The default number of media items to return at a time is 25. The maximum pageSize is 100.
-    items = results.get('mediaItems', [])
-    nextpagetoken = results.get('nextPageToken', '')
-    for item in items:
-        images[item['id']] = item['filename']
 
-    # print(len(items))
-    counter += len(items)
-    print(f"{counter-len(items)} + {len(items)} = {counter}")
-    # break
+if nextpagetoken != 'Dummy':
+    file = open("images.json", "r")
+    images = json.load(file)
+    counter = len(images)
+    print(counter)
+else:
+    images = {}
+    counter = 0 
 
-file = open("images.json", 'w')
-json.dump(images, file)
-file.close()
+
+
+
+try:
+    while nextpagetoken != '':
+        
+        nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
+        
+        try:
+            results = google_photos.mediaItems().list(pageSize=100, pageToken=nextpagetoken).execute()
+        except:
+            results = google_photos.mediaItems().list(pageSize=100, pageToken=nextpagetoken).execute()
+        # results = google_photos.mediaItems().search(body={"filters":  {"dateFilter": {"dates": [{"day": '1', "month": '6', "year": '2021'}]}},
+        #               "pageSize": 100, "pageToken": nextpagetoken}).execute()
+        # The default number of media items to return at a time is 25. The maximum pageSize is 100.
+        items = results.get('mediaItems', [])
+        nextpagetoken = results.get('nextPageToken', '')
+        for item in items:
+            images[item['id']] = item['filename']
+
+        # print(len(items))
+        counter += len(items)
+        print(f"{counter-len(items)} + {len(items)} = {counter}")
+        # break
+finally:
+    print("next token is:", nextpagetoken)
+    file = open("images.json", 'w')
+    json.dump(images, file)
+    file.close()
