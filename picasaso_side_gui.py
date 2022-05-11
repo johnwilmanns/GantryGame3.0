6b29cba282fc4f5d2c9e36a7c93ebdbdc9200d6d
johnwilmanns <johnwilmanns@gmail.com>
2022-05-11 12:50:31 -0700

    tried again

--- picasaso_side_gui.py
+++ picasaso_side_gui.py
@@ -1,6 +1,10 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 import pickle
 global segments
 import run_gantry
@@ -8,10 +12,26 @@ class MainWindow(Screen):
     def enter_code(self):
         #get list form pickle file
         global segments
-        segments = pickle.load(open("/home/soft-dev/Documents/paths/" + self.code.text + ".pkl", "rb"))
-        self.code.text = ""
-        run_gantry.main(segments)
+        try:
+            segments = pickle.load(open("/home/soft-dev/Documents/paths/" + self.code.text + ".pkl", "rb"))
+            self.code.text = ""
+            run_gantry.main(segments)
+        except Exception:
+            layout = GridLayout(cols=1, padding=10)
 
+            popupLabel = Label(text="Invalid Code, Please try again")
+            closeButton = Button(text="Close")
+
+            layout.add_widget(popupLabel)
+            layout.add_widget(closeButton)
+
+            # Instantiate the modal popup and display
+            popup = Popup(title='Failed',
+                          content=layout)
+            popup.open()
+
+            # Attach close button press with popup.dismiss action
+            closeButton.bind(on_press=popup.dismiss)
 
 class SecondWindow(Screen):
     pass
